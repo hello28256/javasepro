70c2706361aa472faf6171f58b6c9c677cdc6331
Mrhelloyang <2131424889@qq.com>
2025-05-13 15:09:05 +0800

    第一次提交

---
+++ day01-oop/src/com/itheima/d1_static_overview/Student.java
@@ -0,0 +1,10 @@
+package com.itheima.d1_static_overview;
+
+public class Student {
+    public static int count;
+
+    public static void study (){
+
+        System.out.println("学生在学习");
+    }
+}
---
+++ day01-oop/src/com/itheima/d1_static_overview/test.java
@@ -0,0 +1,14 @@
+package com.itheima.d1_static_overview;
+
+public class test {
+    public static void main(String[] args){
+    Student.count=20;
+    System.out.println(Student.count);
+    Student.study();
+    }
+}
+
+
+//态度决定高度
+
+
